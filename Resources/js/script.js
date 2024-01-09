8e70c5692aba673dfc3301cf7309632ce920a163
Kristy Koenig <kristykoenig@live.com>
2024-01-09 13:27:28 -0800

    Update

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,21 +1,16 @@
 var mainUrl = "https://kristy-games.github.io/";
 var imagePath = `${mainUrl}Resources/Images/`;
 
-var logoIconUrl = `${imagePath}Logo/Logo.png`;
-var homeIUrl = `${imagePath}Shapes/Home.png`;
-var rpsIconUrl = `${imagePath}AppIcons/.png`;
-var appleImageUrl = "StoreIcons/Apple_Icon.png";
-var googleImageUrl = "StoreIcons/Google_Icon.png";
+var logoImageUrl = `${imagePath}Logo/Logo.png`;
+var homeImageUrl = `${imagePath}Shapes/Home.png`;
+var rpsIconUrl = `${imagePath}AppIcons/RPS_Icon.png`;
+var appleIconUrl = `${imagePath}StoreIcons/Apple_Icon.png`;
+var googleIconUrl = `${imagePath}StoreIcons/Google_Icon.png`;
 
-var logoImageUrl = `${imagePath}${logoImage}`;
-var rpsImageUrl = `${imagePath}${rpsImage}`;
-var appleImageUrl = `${imagePath}${appleImage}`;
-var googleImageUrl = `${imagePath}${googleImage}`;
+var appleStorePath = "https://apps.apple.com/app/";
+var googleStorePath = "https://play.google.com/store/apps/details?id="
 
-var applePath = "https://apps.apple.com/app/";
-var googlePath = "https://play.google.com/store/apps/details?id="
-
-var rpsApple = "block-your-friends/id1611839181";
+var rpsApple = "xcode/id497799835";
 var rpsGoogle = "com.KristyGames.RockPaperScissors";
 
 export function setLogo() {
@@ -29,22 +24,25 @@ export function setMainButtons() {
 	var mainButtons = document.getElementsByClassName("button main");
 
 	Array.from(mainButtons).forEach(function(button) {
-		button.classList.add("text");
 		button.textContent = button.id;
+		button.classList.add("text");
 		button.href = `${mainUrl}${button.id.split(' ').join('')}`;
 		buttonSetup(button);
 	});
 }
 
 function setHomeButton() {
-	var id = "Home Button"
+	var id = "HomeButton"
 	var button = document.getElementById(id);
-	button.href = mainUrl;
-	logo.src = logoImageUrl;
-	logo.className = "image logo"
-	logo.setAttribute("alt", id);
-	//src = home image
-	buttonSetup(button);
+
+	if (button)
+	{
+		button.href = mainUrl;
+		button.src = homeImageUrl;
+		button.className = "button"
+		button.setAttribute("alt", "Home");
+		buttonSetup(button);
+	}
 }
 
 function buttonSetup(button) {
@@ -57,7 +55,8 @@ export function setDownloadButtons() {
 	//get class button download
 	//foreach download button
 
-	//title = "Download on the Apple App Store"
+	//title/alt = "Download on the Apple App Store"
+
 	var rpsAppleUrl = `${applePath}${rpsApple}`; //set href, src = qrcode
 	var rpsGoogleUrl = `${googlePath}${rpsGoogle}`;
 }
