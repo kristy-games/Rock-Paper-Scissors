f6abf2e6555b7b0a7900cf9dd982a25a9eba5bde
Kristy Koenig <kristykoenig@live.com>
2024-01-09 08:59:05 -0800

    Update

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,16 +1,43 @@
-var baseUrl = "https://kristy-games.github.io/";
-var logoPath = "Resources/Images/Logo/Logo.png";
-var logoUrl = `${baseUrl}${logoPath}`;
+var main = "https://kristy-games.github.io/";
+var images = "Resources/Images/";
 
-function setButtonProperties(id) {
-	var button = document.getElementById(id);
-	button.textContent = id;
-	button.title = id;
-	button.target = "_blank";
-	button.href = `${baseUrl}${id.split(' ').join('')}`;
+var appleDownload = "https://apps.apple.com/app/";
+var googleDownload = "https://play.google.com/store/apps/details?id=com."
+
+var rpsAppleDownload = `${appleDownload}TODO`;
+var rpsGoogleDownload = `${googleDownload}KristyGames.RockPaperScissors`;
+
+var logoImage = "Logo/Logo.png";
+var rpsImage = "AppIcons/RPS_Icon.png";
+var appleImage = "StoreIcons/Apple_Icon.png";
+var googleImage = "StoreIcons/Google_Icon.png";
+
+var logoImageUrl = `${baseUrl}${imagePath}${logoImagePath}`;
+var rpsImageUrl = `${baseUrl}${rpsImagePath}`;
+var appleImageUrl = `${baseUrl}${rpsImagePath}`;
+var googleImageUrl = `${baseUrl}${rpsImagePath}`;
+
+function setLogo() {
+    var logo = document.getElementById("Logo");
+    logo.src = logoImageUrl;
 }
 
-export { logoUrl, setButtonProperties };
+function setButtons() {
+	var buttons = document.getElementsByClassName("button");
+
+    Array.from(buttons).forEach(function(button) {
+        button.textContent = button.id;
+        button.title = button.id;
+        button.target = "_blank";
+        button.href = `${baseUrl}${button.id.split(' ').join('')}`;
+    });
+}
+
+function setDownloadLinks() {
+
+}
+
+export { setLogo, setButtons };
 
 // Apple/google store icons
 // apple/google download pageURLs
